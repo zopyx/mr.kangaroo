dabac317d698abec08550ab6522145fb97e5bc32
Andreas Jung <yet@gmx.de>
2016-04-05 18:14:18 +0200

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.0.1'
+version = '0.0.2'
 
 long_description = \
     open(os.path.join("docs", "source", "README.rst")).read() + "\n" + \
