7d61b49d8de72db69bdfa9391c4f7572619b8812
Andreas Jung <yet@gmx.de>
2016-04-05 18:12:23 +0200

    updated

---
+++ mr/kangaroo/__init__.py
@@ -0,0 +1,1 @@
+print 'Hi from mr.kangaroo'
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+long_description = \
+    open(os.path.join("docs", "source", "README.rst")).read() + "\n" + \
+    open(os.path.join("docs", "source", "HISTORY.rst")).read()
+
+setup(name='mr.kangaro',
+      version=version,
+      description="mr.kangaroo",
+      long_description=long_description,
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='kangaroo',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/mr.kangaroo',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['mr'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      )
